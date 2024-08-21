9d0df0657882bdb9af33538d12f13fc14e6c28eb
ashutosh rajput <rajputashutosh81@gmail.com>
2024-08-22 00:16:36 +0530

    Add Dcoker compose and crosconfig.

--- src/main/java/AshutoshRajput/CoursesByMe/Config/CrosConfig.java
+++ src/main/java/AshutoshRajput/CoursesByMe/Config/CrosConfig.java
@@ -8,7 +8,7 @@ public class CrosConfig implements WebMvcConfigurer{
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-           .allowedOrigins("http://localhost:3000")
+           .allowedOrigins("*")
            .allowedMethods("GET", "POST", "PUT", "DELETE")
            .allowedHeaders("*");
     }
